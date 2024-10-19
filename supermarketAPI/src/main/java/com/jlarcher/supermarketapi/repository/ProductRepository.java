737bf61015c3c02505ce5f6764e1672df19a45c5
jordan larcher <lds.larcher@gmail.com>
2024-10-18 23:40:38 -0300

    updating folder supermarketAPI

--- supermarketAPI/src/main/java/com/jlarcher/supermarketapi/repository/ProductRepository.java
+++
@@ -1,7 +0,0 @@
-package com.jlarcher.supermarketapi.repository;
-
-import com.jlarcher.supermarketapi.model.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {
-}
